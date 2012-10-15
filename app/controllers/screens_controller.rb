1dd3907f8a6b58cd12116f14eb31c915dc87e465
ankithbti <ankithbti007@gmail.com>
2012-10-15 08:46:07 +0530

    Checking in base on top of which we will build

---
+++ app/controllers/screens_controller.rb
@@ -0,0 +1,19 @@
+class ScreensController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def faq
+  end
+
+  def privacy
+  end
+
+  def terms
+  end
+
+  def construction
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smartcoach::Application.config.secret_token = 'c635a08fc672ddbdbf74f44884146d33e21e47c466226662ee15ed46aac6250c4c850bc3568ce1b608398e192096afd13b58747532a2af103c8e46b543b2dc4c'
