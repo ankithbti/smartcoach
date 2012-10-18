e2ccc8e0f83cffa5fc119fee1709c3f71951b5b4
ankithbti <ankithbti007@gmail.com>
2012-10-19 02:40:30 +0530

    Changed the db to correct the groupings table for production | order

--- db/migrate/20121018181224_create_groupings.rb
+++ db/migrate/20121018181224_create_groupings.rb
@@ -1,8 +1,9 @@
 class CreateGroupings < ActiveRecord::Migration
   def change
     create_table :groupings do |t|
+    	t.belongs_to :group
     	t.belongs_to :tutorial
-      	t.belongs_to :group
+      	
       t.timestamps
     end
   end
