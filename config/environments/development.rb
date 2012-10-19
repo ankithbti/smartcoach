6947e4c9203733e299945b4c235a4ce356a84da6
ankithbti <ankithbti007@gmail.com>
2012-10-20 01:01:03 +0530

    Added the Mailer Functionality | Created the Mailer NewsletterMailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,22 @@
 Smartcoach::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.action_mailer.default_url_options = { host: "fitied.com" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "fitied.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    # user_name: ENV["GMAIL_USERNAME"],
+    # password: ENV["GMAIL_PASSWORD"]
+    user_name: "fitiedforyou@gmail.com",
+    password: "onlinefitied007@"
+  }
+
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
